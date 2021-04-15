c226fc5bcba96a887d5fbfe0dbec65a4e746af37
Gregor Weindl <g.weindl@pixelpoems.com>
2021-04-15 17:05:18 +0200

    added example bootstrap file

---
+++ Principals/SOLID/good/bootstrap.example.php
@@ -0,0 +1,9 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+
+/* set your db credentials */
+$configuration = [
+    'db_dsn' => 'mysql:host=[your-host];dbname=[your-db]',
+    'db_u' => '[your user]',
+    'db_p' => '[your password]'
+];
